14686b1a4b0f4265d86add9b0bc64fc589886501
YuriyMitrofanov <yurasell@yandex.ru>
2023-05-12 22:44:16 +0300

    Implemented a reusable badge component

---
+++ src/app/components/common/badge.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Badge = ({ color = 'primary', content }) => {
+    return (
+        <span className={`badge bg-${color}`}>{content}</span>
+    );
+}
+
+Badge.propTypes = {
+    color: PropTypes.string,
+    content: PropTypes.string
+}
+
+export default Badge
