40404d11f386b65dd4ed69031a33a1f8c70476d3
Vova <vlasadovoj@gmail.com>
2023-05-12 21:36:47 +0300

    Изменение NavBar, добавление redux и reduxToolkit, добавление eslint и propTypes

--- src/app/components/ui/NavBar.jsx
+++ src/app/components/ui/NavBar.jsx
@@ -3,42 +3,28 @@ import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className='navbar navbar-expand-sm navbar-dark bg-primary mb-2'>
-      <div className='container-fluid'>
-        {/* <NavLink to='/' className='navbar-brand'>
-            Some Brand
-          </NavLink> */}
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarContent'
-          aria-controls='navbarContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        <div className='collapse navbar-collapse' id='navbarContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <NavLink to='/' className='nav-link'>
-                Home
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to='/member/1' className='nav-link'>
-                First member
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to='/favorites' className='nav-link'>
-                Favorites
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    <nav className='bg-primary mb-2'>
+      <div>
+        <ul className='d-flex list-unstyled ml-0 pl-0  me-auto mb-2 p-3'>
+          <li className='me-2 '>
+            <NavLink to='/' className='text-white text-decoration-none'>
+              Home
+            </NavLink>
+          </li>
+          <li className='me-2 '>
+            <NavLink to='/member/1' className='text-white text-decoration-none'>
+              First member
+            </NavLink>
+          </li>
+          <li className='me-2 '>
+            <NavLink
+              to='/favorites'
+              className='text-white text-decoration-none'
+            >
+              Favorites
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   )
