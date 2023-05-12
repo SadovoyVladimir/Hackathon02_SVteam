45b5d740993cae054d4514fe3aaed10fa95a0f05
Vova <vlasadovoj@gmail.com>
2023-05-12 15:01:12 +0300

    Initial

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container mt-2 text-center'>
+      <h1>Hackathon №2</h1>
+      <h2>Initial text</h2>
+    </div>
+  )
+}
+
+export default App
