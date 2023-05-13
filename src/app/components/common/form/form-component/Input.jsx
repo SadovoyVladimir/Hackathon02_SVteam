8968b9a10d845b06dddacd5396acc4c00a857985
Igor <zhigalovzenit112233@gmail.com>
2023-05-13 13:45:23 +0300

    add prop-types to input

--- src/app/components/common/form/form-component/Input.jsx
+++ src/app/components/common/form/form-component/Input.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Input = ({ type, name, id }) => {
   return <input type={type} id={id} name={name} />
 }
 
 export default Input
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
+}
