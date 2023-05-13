26f0a57e256ec1cdb6410fb3dbe55641cb13bf6f
Igor <zhigalovzenit112233@gmail.com>
2023-05-13 11:06:40 +0300

    changes userCard

--- src/app/components/common/button.jsx
+++ src/app/components/common/button.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({ buttonColor = 'primary', handler, buttonName, buttonType }) => {
-
-    return (
-        <button className={`btn btn-${buttonColor}`} onClick={handler} type = {buttonType}>{buttonName}</button>
-    )
+const Button = ({
+  buttonColor = 'primary',
+  handler,
+  buttonName,
+  buttonType
+}) => {
+  return (
+    <button
+      className={`btn btn-${buttonColor}`}
+      onClick={handler}
+      type={buttonType}
+    >
+      {buttonName}
+    </button>
+  )
 }
 
 Button.propTypes = {
-    buttonColor: PropTypes.string,
-    handler: PropTypes.func,
-    buttonName: PropTypes.string,
-    buttonType: PropTypes.string
+  buttonColor: PropTypes.string,
+  handler: PropTypes.func,
+  buttonName: PropTypes.string,
+  buttonType: PropTypes.string
 }
- 
+
 export default Button
