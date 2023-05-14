d4b1c1ab491444c80d215b05147db87544abf394
YuriyMitrofanov <yurasell@yandex.ru>
2023-05-14 16:24:59 +0300

    work copy

--- src/app/components/page/MemberPage.jsx
+++ src/app/components/page/MemberPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import api from '../../../api'
 import UserImageCard from '../ui/userImageCard'
 import UserSocialNetworksCard from '../ui/UserSocialNetworksCard'
 import UserInfoCard from '../ui/UserInfoCard'
 import { useParams } from 'react-router-dom'
+import { getMemberById, getMembers } from '../../store/memberSlice'
 
 export default function MemberPage() {
   const { users } = api
   const { memberId } = useParams()
+  const members = useSelector(getMembers())
+  const selectedMember = useSelector(getMemberById('aYqow2W2oKwFlwyvuJtMm'))
+  console.log('members', members)
+  console.log('member', selectedMember)
   const user = users.find(user => user.id === memberId)
   return (
     <>
